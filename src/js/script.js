1a3e42483cf4ebd46502b611295e8b6d8fafb2c6
Denis Denisov <denis.denisv8701@gmail.com>
2023-09-12 21:00:50 +0200

    created section CATALOG

--- src/js/script.js
+++ src/js/script.js
@@ -18,4 +18,22 @@ $(document).ready(function(){
             }
         ]
     });
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+    function toggleSlide(item){
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__contents').eq(i).toggleClass('catalog-item__contents_active');
+                $('.catalog-item__lists').eq(i).toggleClass('catalog-item__lists_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__contents__link');
+    toggleSlide('.catalog-item__lists__link');
   });
